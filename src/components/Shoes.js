ede95bfa2407b547a1263f7b14d3bd62a64ceca9
Tanzeel <ktanzeel80@gmail.com>
2024-08-06 00:35:03 +0530

    Added landing page

--- src/components/Shoes.js
+++ src/components/Shoes.js
@@ -121,7 +121,7 @@ function Shoes() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="#" className="btn btn-primary">
+            <a href="/" className="btn btn-primary">
               Go somewhere
             </a>
           </div>
@@ -232,7 +232,7 @@ function Shoes() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="#" className="btn btn-primary">
+            <a href="/" className="btn btn-primary">
               Go somewhere
             </a>
           </div>
